8bf6e066cc26e3b34053fb26b3e9ce847c3d9def
pdhende <pdhendework@gmail.com>
2022-05-11 09:06:32 -0400

    Updated the webpack.config.json to configure the entry, output and modules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,22 +8,67 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
+    // Based on the mode either production/development/none the app is set with set configurations
     mode: 'development',
+    // The entry module states which files are used/needed for the entry into the application.
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    // The output modules specifies the names of files to be created and the location.
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
-    ],
 
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        name: 'PWA Text Editor',
+        short_name: 'Text Editor',
+        description: 'Create notes on the go!',
+        background_color: '#7A871E',
+        theme_color: '#7A871E',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
+    ],
+    // This module specifies the 'Loaders' which helps webpack to process other file types(other than js and json)
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel is a compiler that helps newer version like ES6 work on older browsers too.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
