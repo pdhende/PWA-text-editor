5baa9b48fa985440a5e85676b9b562481b951e48
pdhende <pdhendework@gmail.com>
2022-05-13 16:35:46 -0400

    Updated webpack.config.js to fix issue of logo display

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-   console.error('Adding content to JATE database!');
+   console.log('Adding content to JATE database!');
   
    const jateDB = await openDB('jate', 1);
 
