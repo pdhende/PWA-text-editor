886fa180611cfdb9b17e285f401920a87e8f5836
pdhende <pdhendework@gmail.com>
2022-05-12 21:43:04 -0400

    Updated database .js to open connection to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+   console.error('Adding content to JATE database!');
+  
+   const jateDB = await openDB('jate', 1);
+
+  // Transaction to interact with db
+  const jateTransactn = jateDB.transaction('jate', 'readwrite');
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
