5f4c32254f3601692db6a69a36b02bdd71ca7d78
pdhende <pdhendework@gmail.com>
2022-05-15 01:14:53 -0400

    Updated database.js to fix issue regarding getDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,11 +12,12 @@ const initdb = async () =>
     },
   });
 
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-   console.log('Adding content to JATE database!');
-  
-   const jateDB = await openDB('jate', 1);
+  console.log('Adding content to JATE database!');
+
+  const jateDB = await openDB('jate', 1);
 
   // Transaction to interact with db for performing operations like read/write
   const jateTransactn = jateDB.transaction('jate', 'readwrite');
@@ -24,23 +25,22 @@ export const putDb = async (content) => {
   // The object store 'jate' that is created on line 10 is accessed
   const textObj = jateTransactn.objectStore('jate');
 
-  const newText = textObj.add({value : content});
+  const newText = textObj.add({ value: content });
   const result = await newText;
   console.log("Successfully saved the text!", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('Getting content from JATE database!');
-  const jateDB = await openDB('jate', 1);
+  console.log('GET from the database');
 
-  const jateTransactn = jateDB.transaction('jate', 'readonly');
-
+  const jateDb = await openDB('jate', 1);
+  const jateTransactn = jateDb.transaction('jate', 'readonly');
   const textObj = jateTransactn.objectStore('jate');
   const allText = textObj.getAll();
   const result = await allText;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
